3ce067000d0278e6ee13d8548b63cbbb970c03fa
wq0425 <34827777+wq0425@users.noreply.github.com>
2018-06-07 20:42:59 +0800

    Update shopcarData.js

--- react/买车+city/src/mock/data/shopcarData.js
+++ react/买车+city/src/mock/data/shopcarData.js
@@ -1,18 +1,11 @@
 let data = [
-    { title: "校长", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "院长", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "主任", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "讲师", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "辅导员", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "就业指导老师", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "督查", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "教务", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "班长", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "学委", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "纪委", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "安委", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "生委", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
-    { title: "文化书记", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "哈哈哈", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "呃呃呃", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "嗯嗯嗯", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "鹅鹅鹅", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "擦擦擦", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "撒飒飒", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
+    { title: "打大的", price: Math.ceil(Math.random() * 500), count: 1, flag: false },
 ]
 
 export default data
